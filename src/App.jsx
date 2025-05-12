68b311bee35fec2a5a60d80b82ef5241a92167e4
caamomilaa <163582100+caamomilaa@users.noreply.github.com>
2025-05-12 11:49:26 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,49 @@
+import { useState } from 'react';
+
 const App = () => {
-  return <h1>React</h1>;
+  // const [task, setTask] = useState('');
+  // const getTask =
+  return (
+    <>
+      <header>
+        <h1>TODO</h1>
+        <span>lunita</span>
+      </header>
+      <main>
+        <div>
+          <form>
+            <input
+              type='text'
+              id='input-to-do'
+              placeholder='Create a new todo...'
+            />
+          </form>
+        </div>
+        <div>
+          <div>
+            <input type='checkbox' id='checkbox' />
+            <label htmlFor='checkbox'>make a todo app</label>
+          </div>
+        </div>
+        <section>
+          {/* list-section */}
+          <div>
+            <span>1</span>
+            <p>items left</p>
+          </div>
+          <div>
+            <span>Remove Completed</span>
+          </div>
+        </section>
+        <div>
+          {/* filters */}
+          <span>All</span>
+          <span>Completed</span>
+          <span>Acive</span>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default App;
